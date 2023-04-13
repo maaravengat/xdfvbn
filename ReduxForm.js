c58c76c5a0ef31ea5a10bd3cd0a8f75bf878c580
maaravengat <123921347+maaravengat@users.noreply.github.com>
2023-04-13 19:14:26 +0530

    Add files via upload

--- ReduxForm.js
+++ ReduxForm.js
@@ -1,14 +1,11 @@
 
 import React, { useEffect, useState } from 'react'
-import { Field, reduxForm, formValueSelector, change, resetSection } from 'redux-form'
+import { Field, reduxForm, formValueSelector, } from 'redux-form'
 import { reset } from 'redux-form'
 import { useMutation, gql, useQuery } from '@apollo/client'
 import { connect } from 'react-redux'
-import { initialize } from 'redux-form'
-// import wsdfgh  from "../Backend/public/files"
 import Dropzone from 'react-dropzone'
 import axios from 'axios'
-import { url } from 'redux-form-validators'
 
 const ADD_MUTATIONS = gql`
 mutation AddBook($firstname: String!,$lastname: String!,$dob:String!,$email: String! ,$imagename: String!){
@@ -39,6 +36,7 @@ mutation UpdateUser($id: String!,$firstname: String!,$lastname: String!,$dob:Str
      imagename
     }
 }`
+
 const FORM_DATA = gql`
 query languagequery{
     book {
@@ -82,65 +80,64 @@ const validate = values => {
     }
     return errors
 }
+// const renderSelect = ({ input, label, meta: { touched, error }, children }) => (
+//     <div>
+//         <label class="text-primary col-sm-1">{label}: </label>
+//         <select class="col-sm-2 form-control-sm" {...input}>
+//             {children}
+//         </select>
+//         {touched && error && <span class="text-danger">{error}</span>}
+//     </div>
+// );
+// const renderRadio = ({ input, label, meta: { touched, error } }) => (
+//     <div>
+//         <label class='col-sm-2'>
 
+//             {gender.map((label, key) => (
+//                 <p >
+//                     <input type="radio" {...input} value={key} />
+//                     <label> {label.id}</label>
+//                 </p>
+//             ))}
+//             {touched && error && <span class="text-danger">{error}</span>}
+//         </label>
+//     </div>
 
-const language = [
-    { label: 'Tamil', value: 'Tamil' },
-    { label: 'English', value: 'English' },
-    { label: 'Hindi', value: 'Hindhi' },
-
-];
-const gender = [
-    { id: "Male", value: "Male" },
-    { id: "Female", value: "Female" }
-]
-
-const colors = [
-    { id: 'Reading Books', value: 'red' },
-    { id: 'Swimming', value: 'green' },
-    { id: 'Playing', value: 'blue' },
-];
-
-const renderSelect = ({ input, label, meta: { touched, error }, children }) => (
-    <div>
-        <label class="text-primary col-sm-1">{label}: </label>
-        <select class="col-sm-2 form-control-sm" {...input}>
-            {children}
-        </select>
-        {touched && error && <span class="text-danger">{error}</span>}
-    </div>
-);
-const renderRadio = ({ input, label, meta: { touched, error } }) => (
-    <div>
-        <label class='col-sm-2'>
-
-            {gender.map((label, key) => (
-                <p >
-                    <input type="radio" {...input} value={key} />
-                    <label> {label.id}</label>
-                </p>
-            ))}
-            {touched && error && <span class="text-danger">{error}</span>}
-        </label>
-    </div>
-
-);
+// );
 
 
 
-const renderCheckbox = ({ input, label, meta: { touched, error } }) => (
-    <div >
-        <label >
-            {colors.map((label, value) => (
-                <p>
-                    <input type="checkbox" {...input} />
-                    <label>{label.id}</label>
-                </p>
-            ))}
-            {touched && error && <span class="text-danger">{error}</span>}
-        </label>
-    </div>
-);
+// const renderCheckbox = ({ input, label, meta: { touched, error } }) => (
+//     <div >
+//         <label >
+//             {colors.map((label, value) => (
+//                 <p>
+//                     <input type="checkbox" {...input} />
+//                     <label>{label.id}</label>
+//                 </p>
+//             ))}
+//             {touched && error && <span class="text-danger">{error}</span>}
+//         </label>
+//     </div>
+// );
+// const language = [
+//     { label: 'Tamil', value: 'Tamil' },
+//     { label: 'English', value: 'English' },
+//     { label: 'Hindi', value: 'Hindhi' },
+
+// ];
+// const gender = [
+//     { id: "Male", value: "Male" },
+//     { id: "Female", value: "Female" }
+// ]
+
+// const colors = [
+//     { id: 'Reading Books', value: 'red' },
+//     { id: 'Swimming', value: 'green' },
+//     { id: 'Playing', value: 'blue' },
+// ];
+
+
 
 
 const renderField = ({
@@ -152,7 +149,6 @@ const renderField = ({
     <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: 'center' }} >
         <div>
             <label class="text-primary ">{label}:</label >
-
             <input {...input} placeholder={label} type={type} class="form-control-sm col-sm-8 " />
             {touched &&
                 (error && <span class="text-danger">{error}</span>)}
@@ -162,17 +158,40 @@ const renderField = ({
 )
 
 
-let SyncValidationForm = props => {
-    const [imageName, setImageName] = useState(null)
+
+let SyncValidationForm = (props, values) => {
+    const [imageName, setImageName] = useState()
     const [preview, setPreview] = useState(0)
+
+    useEffect(() => {
+        console.log('fdsdfd');
+    }, [props])
+
+    const renderFields = ({
+        input,
+        label,
+        type,
+        meta: { touched, error },
+    }) => {
+        return (
+            <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: 'center' }} >
+            <div>
+
+                {/* <input {...input} type={type} class="form-control-sm col-sm-8 " /> */}
+                
+                {props.imagename ? (<img src={require(`../../public/image/${props.imagename}`)} alt="uploaded image" />) : (<p>Upload Imagesy</p>)}
+                {touched &&
+                    (error && <span class="text-danger">{error}</span>)}
+
+            </div>
+        </div>
+        )
+        
+    }
+
     const RenderUpload = ({ label, input: { value, onChange } }) => {
 
-
-
-
         const onDrop = (acceptedfiles) => {
-
-            // let imageName= null
 
             const fromData = new FormData();
             fromData.append('file', acceptedfiles[0])
@@ -182,14 +201,13 @@ let SyncValidationForm = props => {
                     'Content-Type': 'multipart/form-data'
                 }
             }).then((response) => {
+
                 setImageName(response.data.file.filename)
 
                 if (response.status == 200) {
                     console.log(response.data.file.filename, 'fdsdf');
                     setImageName(response.data.file.filename)
                     console.log(imageName, 'gfdfggfgg');
-
-                    //   imageName = response.data.file.filename
                 }
 
                 const selectedFile = acceptedfiles[0];
@@ -202,11 +220,6 @@ let SyncValidationForm = props => {
                     console.log(reader.result);
                 }
                 reader.readAsDataURL(selectedFile)
-
-                // setImageName(imageName)
-                console.log('=========imageName====', imageName);
-
-
             })
 
 
@@ -217,7 +230,6 @@ let SyncValidationForm = props => {
                     onDrop={onDrop}  >
                     {({ getRootProps, getInputProps }) => (
                         <div class="dropzone mt-4 border-dashed" style={{
-
                         }}
                             {...getRootProps()}>
                             <input {...getInputProps()} />
@@ -231,9 +243,8 @@ let SyncValidationForm = props => {
 
         )
     }
-    const { handleSubmit, reset, resetForm, initialValues, dispatch, } = props
+    const { handleSubmit, reset, dispatch, } = props
 
-    // const { book } = useQuery(FORM_DATA)
     const [addbook] = useMutation(ADD_MUTATIONS, {
         refetchQueries: [
             { query: FORM_DATA }
@@ -251,21 +262,16 @@ let SyncValidationForm = props => {
         refetchQueries: [
             { query: FORM_DATA }
         ],
-
-
     })
 
     const { data } = useQuery(FORM_DATA)
 
-
-
     const handleEdit = (id) => {
-        console.log(id, 'howzat');
         props.initialize(id)
     }
 
     const submit = async values => {
-
+        // console.log(values, 'gfdsdfgfd');
         if (props.id) {
             await updateuser({
                 variables: {
@@ -275,13 +281,12 @@ let SyncValidationForm = props => {
                     dob: props.dob,
                     email: props.email,
                     imagename: imageName
-
                 }
             })
+            // setImageName();
         }
         else {
             await addbook({
-
                 variables: {
                     firstname: values.firstname,
                     lastname: values.lastname,
@@ -289,44 +294,53 @@ let SyncValidationForm = props => {
                     email: values.email,
                     imagename: imageName
                 }
-
             })
         }
-        //  reset()
         dispatch(reset('SyncValidationForm'))
         setPreview(false)
     }
-    useEffect(() => {
-        console.log("userdata");
-    }, [])
     return (
         <div >
-
             <form onSubmit={handleSubmit(submit)} >
                 <h3 class='text-primary'>Redux Form</h3>
                 <br />
-
-
-
                 <Field
                     name="firstname"
                     type="text"
                     component={renderField}
-                    label="FirstName"
-                />
+                    label="FirstName" />
                 <br />
-                <Field name="lastname" type="text" component={renderField} label="LastName" />
+                <Field
+                    name="lastname"
+                    type="text"
+                    component={renderField}
+                    label="LastName" />
                 < br />
-
-                <Field name="dob" type="date" dateformat="yyyy/mm/dd" component={renderField} label="D.O.B" />
+                <Field
+                    name="dob"
+                    type="date"
+                    dateformat="yyyy/mm/dd"
+                    component={renderField}
+                    label="D.O.B" />
                 < br />
-                <Field name="email" type="email" component={renderField} label="Email" />
+                <Field
+                    name="email"
+                    type="email"
+                    component={renderField}
+                    label="Email" />
+                <br />
+                <Field
+                    name="imagename"
+                    type="image"
+                    component={renderFields}
+                    label="image" />
+                <br />
+                <Field
+                    name="image"
+                    type='file'
+                    component={RenderUpload} />
                 <br />
 
-                <Field name="image" type=' file' component={RenderUpload} />
-
-                <br />
-                <br />
                 {/* 
                 <Field
                   name="picture"
@@ -364,23 +378,16 @@ let SyncValidationForm = props => {
                     <button type="submit" class="bg-success text-dark">
                         Submit
                     </button>
-                    {/* <button type="button" onClick={() => update()}>Update</button> */}
                     <button type="button" class="bg-danger text-dark" onClick={() => dispatch(reset(SyncValidationForm))} >
                         Clear Values
                     </button>
-
                 </div>
-                <div>
-
-                </div>
-
             </form>
             <div>
                 <div>
                     <table class="table">
                         <thead>
                             <tr>
-                                {/* <th>Image</th> */}
                                 <th> First Name</th>
                                 <th> Last Name</th>
                                 <th> D.O.B</th>
@@ -392,40 +399,17 @@ let SyncValidationForm = props => {
                         </thead>
                         <tbody>
                             {data && data.book.map(user => {
-
-                                console.log(user, '=========>');
+                                // console.log(user, '=========>');
                                 return (
                                     <tr>
-                                        {/* <td>{<img src={} />}</td> */}
                                         <td>{user.firstname}</td>
-
                                         <td>{user.lastname}</td>
                                         <td>{user.dob}</td>
                                         <td>{user.email}</td>
                                         <td>{user.imagename}</td>
-                                        {/* <td><div>{<img src={`${process.env.PUBLIC_URL}(${user.imagename})`} />}</div></td> */}
-                                        {/* <td>
-    <div style={{backgroundImage: ur}}>
-    </div>
-</td> */}
-
-
-
-
-                                        {/* <td><div>{<img src ={'../'} />}</div></td> */}
-                                        {/* <td><div>{<img src={`${process.env.PUBLIC_URL}(${user.imagename})`} height ='50px'/>}</div></td> */}
-
-                                        {/* <div style={{ backgroundSize: "cover", backgroundImage: "url(../Backend/public/files/1680676255449.jpeg)" }}>
-
-                                        </div> */}
-
-
                                         <td>
-                                            <button onClick={() => handleEdit(user)}>Edit</button>
-
-                                            <button onClick={() => {
-                                                deleteruser({ variables: { id: user.id } });
-                                            }}>Delete</button>
+                                        <button onClick={() => handleEdit(user)}>Edit</button>
+                                        <button onClick={() => {deleteruser({ variables: { id: user.id } });}}>Delete</button>
                                         </td>
                                     </tr>
 
@@ -452,26 +436,9 @@ SyncValidationForm = (reduxForm({
 const selector = formValueSelector('syncValidation')
 
 
-// SyncValidationForm = connect(
-
-//     state => {
-//         const firstname = selector(state, 'firstname')
-//         const lastname = selector(state, 'lastname')
-//         const dob = selector(state, 'dob')
-//         const email = selector(state, 'email')
-//         const id = selector(state, 'id')
-//         return {
-//             firstname,
-//             lastname,
-//             dob,
-//             email,
-//             id
-//         }
-//     }
-
-// )(SyncValidationForm)
 
 const mapStateToProps = (state) => {
+    console.log(state, 'rtyuioknhg');
     const firstname = selector(state, 'firstname')
     const lastname = selector(state, 'lastname')
     const dob = selector(state, 'dob')
@@ -492,11 +459,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return (
         dispatch(reset('SyncValidationForm'))
-
     )
-
-
-
 }
 
 
